2c662d69866fe518366464ac81d77c6f29452bbe
AlexY404 <bookbugalex@gmail.com>
2026-01-30 17:43:06 -0500

    Created swerve and vision using YAGSL

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,9 @@
 package frc.robot;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -15,13 +18,29 @@ import edu.wpi.first.math.controller.PIDController;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static class DrivebaseConstants {
+
+    public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+    public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+    public static final double MAX_SPEED  = Units.feetToMeters(14.5);
+    
+    public static final double WHEEL_LOCK_TIME = 10; //seconds
+
+  }
+
   public static class OperatorConstants {
+
     public static class ShooterConstants {
+
       public static final double SHOOTER_HIGH_SPEED = 300.0;
       public static final double SHOOTER_LOW_SPEED = 60.0;
       public static final double HIGH_DUTY_CYCLE = 0.3;
       public static final double LOW_DUTY_CYCLE = -0.3;
+
     }
+
     public static final int kDriverControllerPort = 0;
  
   }
